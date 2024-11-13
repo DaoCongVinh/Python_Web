6b2d04e54a8e173aefc9c32b63f989d2c0dedc57
TuanKhoaNLU <22130123@st.hcmuaf.edu.vn>
2024-11-13 11:51:10 +0700

    Add Font Awesome fixed-width icons and sizing utilities

--- shop/forms.py
+++ shop/forms.py
@@ -6,8 +6,8 @@ class Contact_Form(forms.ModelForm):
         model = ContactForm
         fields = ['username', 'email', 'subject', 'message']
         widgets = {
-            'username': forms.TextInput(attrs={'placeholder': 'Your Name', 'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'placeholder': 'Nhập tên của bạn', 'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'placeholder': 'E-mail', 'class': 'form-control'}),
-            'subject': forms.TextInput(attrs={'placeholder': 'Subject', 'class': 'form-control'}),
-            'message': forms.Textarea(attrs={'placeholder': 'Your Message', 'cols': 30, 'rows': 10, 'class': 'form-control'}),
+            'subject': forms.TextInput(attrs={'placeholder': 'Tiêu đề', 'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'placeholder': 'Lời nhắn', 'cols': 30, 'rows': 10, 'class': 'form-control'}),
         }
