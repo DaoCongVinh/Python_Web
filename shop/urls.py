176b39bc2b127203e3a1bb2b89d7240037a7d85e
DaoCongVinh <22130325@st.hcmuaf.edu.vn>
2024-11-26 20:25:11 +0700

    update

--- shop/urls.py
+++ shop/urls.py
@@ -1,15 +1,17 @@
+# shop/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('product_list/', views.product_list, name='shop'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('products/', views.product_list, name='product_list'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('blog/', views.blog, name='blog'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('saveContact/', views.saveContact, name='saveContact'),
     path('cart/', views.cart, name='cart'),
-    path('product/<int:id>/', views.product_detail, name='product_detail'),
-    path('saveContact/',views.saveContact, name = 'saveContact'),
-    path('login/',views.login, name="login"),
-    path('register/',views.register, name="register"),
+    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
 ]
