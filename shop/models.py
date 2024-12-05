8c73241afebcc3ae43ff380880992ddf6812d109
TuanKhoaNLU <22130123@st.hcmuaf.edu.vn>
2024-12-05 14:12:14 +0700

    Chỉnh sửa

--- shop/models.py
+++ shop/models.py
@@ -39,7 +39,7 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    """Represents a single product in the cart."""
+    """Từng sản phẩm trong giỏ hàng"""
     cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
